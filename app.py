721bb9e91e798282311e6546638a58f68ef2cd4b
WWarrior64 <115961340+WWarrior64@users.noreply.github.com>
2025-09-28 11:27:21 -0600

    Refactor: Actualización de rutas, vistas y assets para interfaz de usuario
    - Se reorganizaron las carpetas de imágenes: se movieron de app/Imagenes_stockly/ y app/resources a static/images/ para estandarizar la carga de assets.
    - Se actualizó el proyecto para usar Tailwind compilado localmente en lugar del CDN.
    - Actualización de app.py:
      - Registro de blueprints (auth_bp, main_bp, inventario_bp) ajustado a nuevas rutas y endpoints.
      - Corrección de rutas duplicadas para evitar conflictos en auth.login.
    - Modificaciones en routes:
      - main.py: Ajuste de rutas /home, /login, /pedidos, /reportes, /cuenta.
      - Se eliminó la ruta /inventario de main.py para trasladarla a su propio blueprint (inventario.py).
      - inventario.py: Creación del blueprint de inventario.
    - Cambios en vistas:
      - login.html: Integración de Tailwind local, centrado del formulario, enlace para registro de nuevos empleados, y consistencia de estilos con la paleta de colores de la app.
      - register.html: Integración de Tailwind local con formulario de registro completo, inputs con space-y-4, botón de registro y enlace al login centrado, con espacio debajo.
      - home.html e inventario.html: Ajuste de rutas, organización de templates y ajustes en las paletas de colores para mantener consistencia con el nuevo blueprint y consistencia con las demas vistas.

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+import os
+from flask import Flask, redirect, url_for
 from app.routes.main import main_bp
+from app.routes.auth import auth_bp
+from app.routes.inventario import inventario_bp
 
-app = Flask(__name__, template_folder='app/templates')
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))   # carpeta donde está el archivo app.py
+STATIC_DIR = os.path.join(BASE_DIR, 'static')           # <proyecto>/static
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'app', 'templates')  # <proyecto>/app/templates
+
+app = Flask(
+    __name__, 
+    static_folder=STATIC_DIR,
+    template_folder=TEMPLATE_DIR
+    )
 app.register_blueprint(main_bp)
+app.register_blueprint(auth_bp)
+app.register_blueprint(inventario_bp)
+
+@app.route("/")
+def root():
+    return redirect(url_for("auth.login"))
 
 if __name__ == '__main__':
     app.run(debug=True)
