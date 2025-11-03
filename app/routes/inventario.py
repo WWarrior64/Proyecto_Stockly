55e2c92a4105efd196054d4d5905f1872e910f60
WWarrior64 <115961340+WWarrior64@users.noreply.github.com>
2025-11-03 15:33:56 -0600

    Arreglo de la vista de registrar movimiento de inventario con formulario adaptativo y validaciones. Añadir estilos CSS para nuevos elementos y clases de utilidad.

--- app/routes/inventario.py
+++ app/routes/inventario.py
@@ -5,3 +5,7 @@ inventario_bp = Blueprint('inventario', __name__, url_prefix="/inventario")
 @inventario_bp.route('/')
 def inventario():
     return render_template('inventario/inventario.html')
+
+@inventario_bp.route('/registrar')
+def registrar_movimiento():
+    return render_template('inventario/registrar_movimiento.html')
